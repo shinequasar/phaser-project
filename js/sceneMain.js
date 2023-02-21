63fed64556f50a017f1b02d0dcf80358c828c3b4
10017017 <10017017@wjtb.net>
2023-02-21 17:37:05 +0900

    [feat] 그래픽 동그라미 그리기

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -4,26 +4,33 @@ class SceneMain extends Phaser.Scene {
     }
     preload(){
         //이미지나 소리를 로드하는 곳
-        this.load.spritesheet('boy', 'images/boy.png', { frameWidth: 120, frameHeight: 200 });
-        this.load.image("face","images/face.png");
+        // this.load.spritesheet('boy', 'images/boy.png', { frameWidth: 120, frameHeight: 200 });
+        // this.load.image("face","images/face.png");
     }
     create(){
         //객체들을 정의하는 것
-        this.face = this.add.image(game.config.width/2,500,"face");
-        this.face.setInteractive();
-        this.face.on('pointerdown', this.onDown,this);
-        this.face.on('pointerup', this.onUp,this);
+        // this.face = this.add.image(game.config.width/2,500,"face");
+        // this.face.setInteractive();
+        // this.face.on('pointerdown', this.onDown,this);
+        // this.face.on('pointerup', this.onUp,this);
 
-        this.char = this.add.sprite(0, game.config.height/2, "boy");
-        var frameNames= this.anims.generateFrameNumbers('boy'); //밑에 key,frame을 생략하고 이렇게 만들어도 됨
-        this.anims.create({
-            key: 'walk',
-            frames: frameNames,
-            frameRate: 16, //속도
-            repeat: -1 //반복횟수 (-1로 두면 영원히 움직임)
-        });
-        this.char.play('walk');
-        this.doWalk();
+        //동그라미 그리기
+        this.graphics= this.add.graphics();
+        this.graphics.lineStyle(8, 0xff0000);
+        this.graphics.fillStyle(0xff00ff, .5);
+        this.graphics.strokeCircle(100,200,60);
+        this.graphics.fillCircle(100,200,60);
+
+        // this.char = this.add.sprite(0, game.config.height/2, "boy");
+        // var frameNames= this.anims.generateFrameNumbers('boy'); //밑에 key,frame을 생략하고 이렇게 만들어도 됨
+        // this.anims.create({
+        //     key: 'walk',
+        //     frames: frameNames,
+        //     frameRate: 16, //속도
+        //     repeat: -1 //반복횟수 (-1로 두면 영원히 움직임)
+        // });
+        // this.char.play('walk');
+        // this.doWalk();
 
 
     //     var textConfig={
