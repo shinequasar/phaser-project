033b363dcbbe2146229f29482f9972cd6856ace9
10017017 <10017017@wjtb.net>
2023-02-21 17:28:59 +0900

    [feat] 이미지 클릭시 밝기변화

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -5,9 +5,15 @@ class SceneMain extends Phaser.Scene {
     preload(){
         //이미지나 소리를 로드하는 곳
         this.load.spritesheet('boy', 'images/boy.png', { frameWidth: 120, frameHeight: 200 });
+        this.load.image("face","images/face.png");
     }
     create(){
         //객체들을 정의하는 것
+        this.face = this.add.image(game.config.width/2,500,"face");
+        this.face.setInteractive();
+        this.face.on('pointerdown', this.onDown,this);
+        this.face.on('pointerup', this.onUp,this);
+
         this.char = this.add.sprite(0, game.config.height/2, "boy");
         var frameNames= this.anims.generateFrameNumbers('boy'); //밑에 key,frame을 생략하고 이렇게 만들어도 됨
         this.anims.create({
@@ -19,28 +25,26 @@ class SceneMain extends Phaser.Scene {
         this.char.play('walk');
         this.doWalk();
 
-        // this.text1 = this.add.text(
-        //     game.config.width/2,
-        //     game.config.height/2,
-        //     "HELLO I'M WANT",
-        //     {fontFamily:'Anton', color:'#aa00ff', fontSize:'40px'});
-        // this.text1.setOrigin(.5,.5);
 
-        var textConfig={
-            fontFamily:'Anton',
-            fontSize:'40px',
-            color:'#e2fe0b'
-        }
-            this.text1=this.add.text(
-                game.config.width/2,
-                game.config.height/2,
-                "HELLO I'M WANT",
-                textConfig);
-                
-            this.text1.setOrigin(.5,.5);
-            this.text1.setStroke(white,1);
-            this.text1.setShadow(2,2,white,1,false,true);
+    //     var textConfig={
+    //         fontFamily:'Anton',
+    //         fontSize:'30px',
+    //         color:'#e2fe0b'
+    //     }
+    //         this.text1=this.add.text(
+    //             game.config.width/2,
+    //             game.config.height/2,
+    //             "HELLO I'M WANT",
+    //             textConfig);
+
+    //         this.text1.setOrigin(.5,.5);
+    //         this.text1.setStroke(white,1);
+    //         this.text1.setShadow(2,2,white,1,false,true);
     }
+    onDown(){this.face.alpha=.5;}
+    onUp(){this.face.alpha=1;}
+
+
     doWalk(){
         this.tweens.add({
             targets: this.char, 
@@ -57,7 +61,6 @@ class SceneMain extends Phaser.Scene {
         char.y = game.config.height/2;
         char.alpha =1;
         this.doWalk();
-        // console.log("완성!");
     }
     update(){
         //조건문이나 반복문 정의
