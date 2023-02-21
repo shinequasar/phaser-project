a62b7da04a9e292a50da32910f204fccee38e9cd
10017017 <10017017@wjtb.net>
2023-02-21 15:43:34 +0900

    [feat] 이미지를 스프라이트로 (key, frame)

--- js/sceneMain.js
+++ js/sceneMain.js
@@ -4,25 +4,25 @@ class SceneMain extends Phaser.Scene {
     }
     preload(){
         //이미지나 소리를 로드하는 곳
-        this.load.image("wj", "images/wj.png");
+        this.load.spritesheet('boy', 'images/boy.png', { frameWidth: 120, frameHeight: 200 });
     }
     create(){
         //객체들을 정의하는 것
-        console.log("Ready!");
-        this.face = this.add.image(0, 0, "wj");
-
-        //전체 게임 씬 크기에서 
-        this.face.x = game.config.width/2;
-        this.face.y = game.config.height/2;
-
-
-        // this.face.alpha = .5; //투명도
-        // this.face.scaleX = .5; //크기
-        // this.face.scaleY = .5; //크기
-
-        //가로세로 정비율 만들기
-        // this.face.displayWidth = 100;
-        // this.face.scaleX = this.face.scaleY;
+        this.char = this.add.sprite(game.config.width/2, game.config.height/2, "boy");
+        this.anims.create({
+            key: 'walk',
+            frames: [
+                { key: 'boy',frame:0 },
+                { key: 'boy',frame:1 },
+                { key: 'boy',frame:2 },
+                { key: 'boy',frame:3 },
+                { key: 'boy',frame:4 },
+                { key: 'boy',frame:5 },
+            ],
+            frameRate: 16, //속도
+            repeat: -1 //반복횟수 (-1로 두면 영원히 움직임)
+        });
+        this.char.play('walk');
     }
     update(){
         //조건문이나 반복문 정의
