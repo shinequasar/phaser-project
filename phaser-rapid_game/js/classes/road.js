3cc06503012d9f3884d2cd24f40d80cc6db76fcb
10017017 <10017017@wjtb.net>
2023-02-27 14:36:52 +0900

    [feat] 도로 무한스크롤배경

--- phaser-rapid_game/js/classes/road.js
+++ phaser-rapid_game/js/classes/road.js
@@ -6,10 +6,33 @@ class Road extends Phaser.GameObjects.Container{
         this.add(this.back);
         this.scene.add.existing(this);
 
-        this.back.displayWidth = game.config.width * .5;
-        this.back.scaleY = this.back.scaleX;
-
+        // this.back.displayWidth = game.config.width * .5;
+        // this.back.scaleY = this.back.scaleX;
+        Align.scaleTodGameW(this.back, .5);
         this.setSize(this.back.displayWidth, game.config.height);
         console.log(this);
+
+        this.lineGroup = this.scene.add.group();
+        this.count = 0;
+    }
+    makeLines(){
+        this.vSpace = this.displayHeight/10;
+        for( var i=0; i<20; i++){
+            var line = this.scene.add.image(this.x, this.vSpace*i, "line");
+            line.oy = line.y; //원래 위치 기록 (오리지널 y)
+            this.lineGroup.add(line);
+        }
+    }
+    moveLines(){
+        this.lineGroup.children.iterate(function(child){
+            child.y+=this.vSpace/20;  //속도조절
+        }.bind(this)); //this가 scene을 가리키도록 함
+        this.count++;
+        if(this.count == 20){
+            this.count = 0;
+            this.lineGroup.children.iterate(function(child){
+                child.y = child.oy;
+            }.bind(this));
+        }
     }
 }
--- phaser-rapid_game/js/sceneMain.js
+++ phaser-rapid_game/js/sceneMain.js
@@ -5,15 +5,14 @@ class SceneMain extends Phaser.Scene {
     preload(){
        this.load.image("road", "/phaser-rapid_game/images/road.jpg");
        this.load.image("cars", "/phaser-rapid_game/images/cars.png",{frameWidth:60, frameHeight:126});
+       this.load.image("line", "/phaser-rapid_game/images/line.png");
     }
     create(){
-      var road = new Road({scene:this});
-      road.x = game.config.width/2;
-
-      var car = this.add.image(0,0,"cars");
-      Align.center(car);
+      this.road = new Road({scene:this});
+      this.road.x = game.config.width/2;
+      this.road.makeLines();
     }
     update(){
-    
+      this.road.moveLines();
     }
 }
